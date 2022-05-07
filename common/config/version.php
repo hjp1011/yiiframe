ee872f57bc36a6c31789e41b22330d812edcdc3d
hjp1011@163.com <hjp1011@163.com>
2022-05-07 14:59:55 +0800

    更新版本号到2.1.5

--- common/config/version.php
+++ common/config/version.php
@@ -2,8 +2,8 @@
 
 return [
     'authorization' => Yii::t('app','个人版'),
-    'version' => '2.1.4',
-    'updatetime' => '202205011230',
+    'version' => '2.1.5',
+    'updatetime' => '202205071458',
     'cate' => 1,
     'copyright' => '<a href="http://www.yiiframe.com" target="_blank">YiiFrame</a>',
 ];
